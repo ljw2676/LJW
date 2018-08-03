7c4e7cabb6e7edc793869658a4bafad4f009599e
JungEunee <woua3864@daum.net>
2018-08-03 22:02:14 +0900

    import commandMap 수정

--- YOGI/src/main/java/yogi/admin/member/AdminMemberController.java
+++ YOGI/src/main/java/yogi/admin/member/AdminMemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import yogi.common.common.CommandMap;
+import yogi.config.CommandMap;
 
 
 
