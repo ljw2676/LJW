824e960a3278bc73c59457872432187eb8f5dfe7
JungEunee <woua3864@daum.net>
2018-08-23 10:57:32 +0900

    test -> members

--- YOGI/src/main/java/yogi/main/MainDAO.java
+++ YOGI/src/main/java/yogi/main/MainDAO.java
@@ -11,21 +11,22 @@ import yogi.members.MembersModel;
 @Repository ("mainDAO")
 public class MainDAO extends AbstractDAO{
 	public MembersModel getInterest(String id) {
-		return (MembersModel)selectOne("test.check", id);
+		return (MembersModel)selectOne("members.check", id);
 	}
 	
 	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> fav_field(Map<String, Object> map) throws Exception{
-		return (List<Map<String, Object>>)selectList("test.fav_field", map);
+		return (List<Map<String, Object>>)selectList("members.fav_field", map);
 	}
 	
 	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> fav_area(Map<String, Object> map) throws Exception{
-		return (List<Map<String, Object>>)selectList("test.fav_area", map);
+		return (List<Map<String, Object>>)selectList("members.fav_area", map);
 	}
 	
+	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> all(){
-		return (List<Map<String, Object>>)selectList("test.all");
+		return (List<Map<String, Object>>)selectList("members.all");
 	}
 	
 	
--- YOGI/src/main/java/yogi/members/MembersDAO.java
+++ YOGI/src/main/java/yogi/members/MembersDAO.java
@@ -1,7 +1,5 @@
 package yogi.members;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Repository;
 
 import yogi.common.dao.AbstractDAO;
@@ -10,38 +8,38 @@ import yogi.common.dao.AbstractDAO;
 public class MembersDAO extends AbstractDAO{
 	
 	public void insertMember(MembersModel model){
-		insert("test.insertMember", model);
+		insert("members.insertMember", model);
 	}
 	
 	public MembersModel loginCheck(MembersModel model){
-		return (MembersModel)selectOne("test.loginCheck", model);
+		return (MembersModel)selectOne("members.loginCheck", model);
 	}
 	
 	public int idCheck(String id) {
-		return (Integer)selectOne("test.idCheck", id);
+		return (Integer)selectOne("members.idCheck", id);
 	}
 	
 	public MembersModel findId(MembersModel model) {
-		return (MembersModel)selectOne("test.findId", model);
+		return (MembersModel)selectOne("members.findId", model);
 	}
 	
 	public MembersModel findPW(MembersModel model) {
-		return (MembersModel)selectOne("test.findPW", model);
+		return (MembersModel)selectOne("members.findPW", model);
 	}
 	
 	public void resetPW(MembersModel model) {
-		update("test.resetPW", model);
+		update("members.resetPW", model);
 	}
 	
 	public MembersModel mInfo(int no) {
-		return (MembersModel) selectOne("test.membersInfo", no);
+		return (MembersModel) selectOne("members.membersInfo", no);
 	}
 	
 	public void updateMember(MembersModel model){
-		update("test.updateMembers", model);
+		update("members.updateMembers", model);
 	}
 	public void deleteMember(String name) {
-		update("test.deleteMembers", name);
+		update("members.deleteMembers", name);
 	}
 
 }
